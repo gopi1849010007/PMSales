48b8dca513186614993f481e7f54d23d2d4fca3e
Gopi Nishaad <kumargopi046@gmail.com>
2023-09-04 11:47:27 +0530

    image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from "./components/Header";
 import Hello from "./components/Hello";
 import "./App.css";
+import bulb from "./bulb.jpg";
 export default function App() {
   var x = 6;
   function item(x) {
     return `I have ${x} apples`;
+  }
+
+  function Logo() {
+    const picture = <img src={bulb} alt="bulbimage" />;
+    return picture;
   }
   return (
     <div className="App">
@@ -12,6 +18,7 @@ export default function App() {
       <p>{item(x)}</p>
       <p>{5 < x ? "Yes its sufficient" : "Some more fruits req"}</p>
       <Header />
+      <Logo />
     </div>
   );
 }
